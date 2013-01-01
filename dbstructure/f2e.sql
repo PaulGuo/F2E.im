d825e74f209588417bf872226289914edbad1ee3
GuoKai <badkaikai@gmail.com>
2013-01-01 13:24:54 +0800

    added dbstructure.

---
+++ dbstructure/f2e.sql
@@ -0,0 +1,180 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50527
+ Source Host           : localhost
+ Source Database       : f2e
+
+ Target Server Type    : MySQL
+ Target Server Version : 50527
+ File Encoding         : utf-8
+
+ Date: 01/01/2013 13:22:48 PM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `favorite`
+-- ----------------------------
+DROP TABLE IF EXISTS `favorite`;
+CREATE TABLE `favorite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `owner_user_id` int(11) DEFAULT NULL,
+  `involved_type` int(11) DEFAULT NULL,
+  `involved_topic_id` int(11) DEFAULT NULL,
+  `involved_reply_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `node`
+-- ----------------------------
+DROP TABLE IF EXISTS `node`;
+CREATE TABLE `node` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `slug` text,
+  `thumb` text,
+  `introduction` text,
+  `created` text,
+  `updated` text,
+  `plane_id` int(11) DEFAULT NULL,
+  `topic_count` int(11) DEFAULT NULL,
+  `custom_style` text,
+  `limit_reputation` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `notification`
+-- ----------------------------
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE `notification` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `status` int(11) DEFAULT NULL,
+  `involved_type` int(11) DEFAULT NULL,
+  `involved_user_id` int(11) DEFAULT NULL,
+  `involved_topic_id` int(11) DEFAULT NULL,
+  `involved_reply_id` int(11) DEFAULT NULL,
+  `trigger_user_id` int(11) DEFAULT NULL,
+  `occurrence_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `plane`
+-- ----------------------------
+DROP TABLE IF EXISTS `plane`;
+CREATE TABLE `plane` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `created` text,
+  `updated` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `reply`
+-- ----------------------------
+DROP TABLE IF EXISTS `reply`;
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `topic_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `content` text,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `up_vote` int(11) DEFAULT NULL,
+  `down_vote` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `topic`
+-- ----------------------------
+DROP TABLE IF EXISTS `topic`;
+CREATE TABLE `topic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` text,
+  `content` text,
+  `status` int(11) DEFAULT NULL,
+  `hits` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `node_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `reply_count` int(11) DEFAULT NULL,
+  `last_replied_by` text,
+  `last_replied_time` datetime DEFAULT NULL,
+  `up_vote` int(11) DEFAULT NULL,
+  `down_vote` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `transaction`
+-- ----------------------------
+DROP TABLE IF EXISTS `transaction`;
+CREATE TABLE `transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL,
+  `reward` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `current_balance` int(11) DEFAULT NULL,
+  `involved_user_id` int(11) DEFAULT NULL,
+  `involved_topic_id` int(11) DEFAULT NULL,
+  `involved_reply_id` int(11) DEFAULT NULL,
+  `occurrence_time` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `email` text,
+  `password` text,
+  `username` text,
+  `nickname` text,
+  `avatar` text,
+  `signature` text,
+  `location` text,
+  `website` text,
+  `company` text,
+  `role` int(11) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `reputation` int(11) DEFAULT NULL,
+  `self_intro` text,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `twitter` text,
+  `github` text,
+  `douban` text,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `vote`
+-- ----------------------------
+DROP TABLE IF EXISTS `vote`;
+CREATE TABLE `vote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status` int(11) DEFAULT NULL,
+  `involved_type` int(11) DEFAULT NULL,
+  `involved_user_id` int(11) DEFAULT NULL,
+  `involved_topic_id` int(11) DEFAULT NULL,
+  `involved_reply_id` int(11) DEFAULT NULL,
+  `trigger_user_id` int(11) DEFAULT NULL,
+  `occurrence_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
